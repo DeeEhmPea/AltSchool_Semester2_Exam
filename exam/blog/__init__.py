45e03525af7fbc7ecb691e028425d0cb9229bb1e
Kolawole Durowaiye <durowaiyekolawole@gmail.com>
2022-11-08 21:14:13 +0000

    edit db

--- exam/blog/__init__.py
+++ exam/blog/__init__.py
@@ -9,12 +9,11 @@ DB_NAME = "database.db"
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = "helloworld"
-    
-    from .views import views
-    from .auth import auth
-
     app.config['SQL_ALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
+
+    from .views import views
+    from .auth import auth
 
     app.register_blueprint(views, url_prefix="/")
     app.register_blueprint(auth, url_prefix="/")
